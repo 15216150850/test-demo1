d1f675e358483c85b71aeee89889b44698517ee4
ZXK <ZXK@mail.com>
2018-08-01 14:02:10 +0800

    ataaeasta

--- src/Demo.java
+++ src/Demo.java
@@ -1,24 +1,5 @@
 public class Demo {
-   
-   
-   public static void mian(){
-   
-   
-   }
-   
    public static void main(String[] args ){
-      
-      show()11111;
-   }
 
-   
-   public void show()1{
-   
-   System.out.println("aaaa");
-   }
-    
-   public void show()11111{
-   
-   System.out.println("bbbb");
    }
 }
